7b12ffa920a72f32cc7b89de813326ea2fcbec94
Ramazan Fatih Kocademir <ramazankocademir@gmail.com>
2021-11-28 17:01:11 +0300

    Graphql
    Graphql API fetching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Pokecard from './Components/Pokecard/Pokecard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="containter-fluid">
+       <div class="row align-items-center">
+          <Header/>
+       </div>
+       <div class="row align-items-center">
+          <Pokecard/>
+        </div> 
+      </div>
     </div>
   );
 }
